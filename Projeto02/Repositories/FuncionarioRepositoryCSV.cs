91061ce62249b7877bcccc9340cbd544735a908d
Thiago Oliveira <thiagooliveira1345@hotmail.com>
2022-10-14 15:02:04 -0300

    first commit

---
+++ Projeto02/Repositories/FuncionarioRepositoryCSV.cs
@@ -0,0 +1,37 @@
+﻿using Projeto02.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto02.Repositories
+{
+    public class FuncionarioRepositoryCSV : FuncionarioRepository
+    {
+        public override void ExportarDados(Funcionario funcionario)
+        {
+            #region Definindo o nome e local do arquivo 
+            var path = "c:\\temp\\funcionarios.csv";
+            #endregion
+
+            #region Gravando o conteúdo do arquivo
+
+            using (var streamWriter = new StreamWriter(path, true))
+            {
+                var texto = $"{funcionario.Id};";
+                          texto = $"{funcionario.Nome}";
+                          texto =  $"{funcionario.DataNascimento.ToString("dd/MM/yyyy")}";
+                          texto = $"{funcionario.Cpf}";
+                          texto = $"{funcionario.Matricula}";
+                          texto = $"{funcionario.DataAdmissao.ToString("dd/MM/yyyy")}";
+                          texto = $"{funcionario.Setor.Sigla}";
+                          texto = $"{funcionario.Setor.Descricao}";
+
+                streamWriter.WriteLine(texto);
+            }
+
+            #endregion
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Projeto C# Console para exportação de dados de funcionários. 
+
+## Conteúdo do projeto 
+* Herança e associação de entidades 
+* Classes abstratas 
+* Polimorfismo 
+* Desenvolvimento em camadas 
+* SOLID (SRP, OCP)
